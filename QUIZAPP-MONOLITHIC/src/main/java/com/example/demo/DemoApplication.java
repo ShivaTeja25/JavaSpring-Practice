92b9f2bbf49ca91d6e3fd3d6ed961efb9bec9542
Shiva Teja <84003715+ShivaTeja25@users.noreply.github.com>
2025-02-28 15:41:12 -0500

    Update DemoApplication.java

--- QUIZAPP-MONOLITHIC/src/main/java/com/example/demo/DemoApplication.java
+++ QUIZAPP-MONOLITHIC/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//pg_ctl -D "C:\Program Files\PostgreSQL\17\data" start
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
